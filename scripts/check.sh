fc35a9c5c4b72c20b5a0674b6b0dc42273a0e7b2
sergei-saenko <sergei.saenko@hotmail.com>
2017-05-12 01:16:12 +0300

    minor changes to check.sh

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,10 +1,34 @@
 #!/usr/bin/sh
 NAME=check.sh
-VERSION=0.1
+VERSION=0.3
 HOST=c3
+USER=root
+PASS=explorer
+mysql="/usr/bin/mysql -h $HOST -u$USER -p$PASS -e"
 
+#1 select server/group_of_servers
+#2 select admin
+#3 select system-number
+#4 count servers by customers - quantity/percantage (descending)
+#5 count servers by primary admin (descending)
+#6 remove server from base
+#7 add new server to the base (in case it doesn't exist)
 
-read -p "Enter hostname" hostname
-mysql -h c3 -uroot -pexplorer < select * from list.servers where hostname like "$hostname"
+case $1 in
 
+h|host)  read -p "Enter hostname: " hostname
+    if [ -n $hostname ]
+        then
+        $mysql "select * from list.servers where hostname like '$hostname'"
+    fi
+;;
 
+a|admin)  read -p "Enter surname: " surname
+    if [ -n $surname ]
+        then
+        $mysql "select * from list.servers where primary_admin like '$surname'"
+    fi
+;;
+*) echo "oops, something went wrong..."
+;;
+esac

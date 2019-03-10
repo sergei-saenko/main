78e9ebc6dfd93c1e8a05e3bea78c79cccd3fc1d5
Sergei Saenko <Sergei.Saenko@infopulse.com>
2019-03-10 18:42:36 +0200

    old vagrant directory was removed completely

--- vagrant/centos-7.1/scripts/lamp.sh
+++
@@ -1,3 +0,0 @@
-yum update -y
-yum -y install mariadb mariadb-server httpd vim yum-utils epel-release
-yum-config-manager --disable epel
--- vagrant/scripts/mariadb.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-yum -y install mariadb-server mariadb
-systemctl start mariadb
-systemctl enable mariadb

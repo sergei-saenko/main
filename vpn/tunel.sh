9159d040d9d69c4f478724e4bcae121fa5f2579a
Sergei Saenko <sergei.saenko@hotmail.com>
2019-09-27 01:22:30 +0300

    tunel script was changed to case statement

--- vpn/tunel.sh
+++ vpn/tunel.sh
@@ -1,20 +1,60 @@
-#!/usr/bin/expect -f
-set timeout -1
-set USER "et0529"
-set PASS "!Nimelig.2020"
+#!/bin/bash
 
-case $1 in 
+#Color block
+GREEN='\033[1;32m'
+YELLOW='\033[0;33m'
+RED='\033[1;31m'
+UNDLN_RED='\033[4;31m'
+BOLD_WHITE='\033[1m'
+BLACK_ON_RED='\033[1;30;41m'
+BLACK_ON_YELLOW='\033[1;30;43m'
+BLUE='\033[1;34m'
+NC='\033[0m'
+UKR="\033[1;37;42m"
+NEUTRAL="\033[0;39;49m"
 
 
-	s|start)	spawn ssh $USER@ilp-alfine -q -f -n -o ExitOnForwardFailure=yes -C -2 -N -D 127.0.0.1:12345
+USER="et0529"
+PASS="!Nimelig.2020"
+PROCESS="`ps -ef|grep ExitOnForwardFailure|grep -v grep|awk '{print $2}'`"
+case $1 in
+
+	c|connect)	echo -e "${BOLD_WHITE}TUNNEL: connecting...\n"	
+			sleep 1
+			if [ -n "$PROCESS" ]	
+			then echo -e "\n${BOLD_WHITE}Tunnel is already running: ${GREEN}$PROCESS\n"
+			else echo -e "\n${BLUE}Starting tunel..."
+			/usr/bin/expect <<- DONE
+			set timeout -1
+			spawn ssh $USER@ilp-alfine -q -f -n -o ExitOnForwardFailure=yes -C -2 -N -D 127.0.0.1:12345
 			expect "et0529@ilp-alfine's password:"
-			send "$PASS\r"
-			interact
-		;;
+			send -- "$PASS"
+			send -- "\r"
+			expect eof
+			DONE
+			echo -e "\n${NEUTRAL}Tunnel has started: ${GREEN}`ps -ef|grep ExitOnForwardFailure|grep -v grep|awk '{print $2}'`\n"
+			fi
+	;;
 
-	stop)		/bin/kill -9 `ps -ef|grep 'ExitOnForwardFailure'|grep -v grep|awk '{print $2}'`
-		;;
+	d|disconnect) 	echo -e "${BOLD_WHITE}TUNNEL: disconecting...\n"
+			sleep 1
+			if [ -n "$PROCESS" ]
+			then /bin/kill -9 $PROCESS
+			echo -e "${NEUTRAL}Tunnel is closed\n"
+			else echo -e "${NEUTRAL}Tunel is not running\n"
+			fi
+	;;
 
-	*)		echo -e "usage: tunel {start|stop}\n"
-		;;
+	s|status) 	echo -e "${BOLD_WHITE}TUNNEL: checking status...\n"
+			sleep 1
+			if [ -n "$PROCESS" ]
+			then echo -e "${GREEN}Running: ${GREEN}$PROCESS\n"
+			else echo -e "${YELLOW}Not running\n"
+			fi
+
+	;;
+
+	*)	echo -e "${BOLD_WHITE}USAGE: ${NC}tunel {c|connect;d|disconnect;s|status}\n"
+	;;
+
 esac
